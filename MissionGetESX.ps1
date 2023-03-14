a77745d1acc09040b46e3e1a6a166acf0fd59a07
Axel-Danglot <92787608+Axel-Danglot@users.noreply.github.com>
2023-03-14 09:31:15 +0100

    Update MissionGetESX.ps1

--- MissionGetESX.ps1
+++ MissionGetESX.ps1
@@ -3,12 +3,12 @@
 #Date de Création : 24/05/2022
 
 ######## VARIABLE A CHANGER ###########
-#$ip = 10.59.3.185
-#$User = administrator@maquette.lan
-#$Psswd = Axians2022$
+#$ip =
+#$User = 
+#$Psswd = 
 #######################################
 # même chose pour HyperV 
 Set-PowerCLIConfiguration -InvalidCertificateAction ignore
-Connect-VIServer -Server 10.59.3.185 -User administrator@maquette.lan -Password Axians2022$
+Connect-VIServer -Server $ip -User $User -Password $Psswd
 Get-VM | Where-object {$_.powerstate -eq "poweredon"} | select Name,PowerState,MemoryGB,NumCpu,UsedSpaceGB
 Get-VM | Where-object {$_.powerstate -eq "poweredon"} | select Name,PowerState,MemoryGB,NumCpu,UsedSpaceGB | Export-Csv -path “C:\test\vminventory.csv” -NoTypeInformation
